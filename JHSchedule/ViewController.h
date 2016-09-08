54d7a518c50d08b8a7f8e56b2e244a01561f4c22
Jiahong Xu <xjh_28@163.com>
2016-09-08 12:00:40 +0800

    Initial Commit

---
+++ JHSchedule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHSchedule
+//
+//  Created by 307A on 16/9/8.
+//  Copyright © 2016年 徐嘉宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JHSchedule/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JHSchedule
+//
+//  Created by 307A on 16/9/8.
+//  Copyright © 2016年 徐嘉宏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
